e15206cab0093a78dc5ee09752946ebb69e6a3f2
rhylvin2022 <rhylvin2018@gmail.com>
2023-11-24 17:04:34 +0800

    persona internal

---
+++ persona_flutter-3.1.0/lib/src/core/enums.dart
@@ -0,0 +1,8 @@
+/// Source for theme value
+enum InquiryThemeSource {
+  /// Instructs the SDK to use theme values set in the Persona Dashboard
+  server,
+
+  /// Uses theme values set on client
+  client,
+}
